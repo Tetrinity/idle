9f5369d327c06fb999dc8af2a8b7f76dc2ced01f
Tetrinity <zephyrsg@hotmail.co.uk>
2015-12-20 17:37:58 +0000

    Added Bootstrap tabs

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('idle.routing', ['ngRoute'])
-angular.module('idle.controller', [])
+angular.module('idle.controller', ['ui.bootstrap'])
 angular.module('idle.service', ['ngCookies'])
 
 var app = angular.module('idle', ['idle.routing', 'idle.controller', 'idle.service'])
